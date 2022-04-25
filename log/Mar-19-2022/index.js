67327350f6a2e66b46d74199515bc07837d8af09
Rajesh <rajesh.knock@gmail.com>
2022-04-25 18:04:35 +0530

    exercise

--- log/Mar-19-2022/index.js
+++ log/Mar-19-2022/index.js
@@ -1,6 +1,21 @@
-let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let length = text.length;
-console.log(length);
-console.log("test")
-console.log("msg");
+let num1 = 1
+let num2 = 2
+console.log(num1+num2)
+let person = {name:"rajesh",
+age:25,
+}
+console.log(person.age)
+console.log(person['name'])
+let favcolors = ["red","yellow","blue"]
+console.log(favcolors.length)
+let name = "rajesh";
+let msg = "my name is"+name+"& i'm learning javascript";
+console.log(msg)
+let str = "apple,banana,kiwi";
+let part = str.substr(6,6);
+let text1 = "hello";
+let text2 = "sir";
+let text3 = text1.concat("",text2);
+
+
 
